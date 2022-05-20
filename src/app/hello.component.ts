6e599027ac0a71094f2e7ea63e1e15b60e117d8e
riccardoperra <perrariccardo0@gmail.com>
2022-05-20 23:44:55 +0200

    2 files modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,8 +1,12 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  InjectFlags,
+  InjectionToken,
   Input,
   OnInit,
+  Optional,
+  SkipSelf,
   ɵɵdirectiveInject,
 } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
@@ -10,10 +14,30 @@ import { BehaviorSubject } from 'rxjs';
 import { finalize, tap } from 'rxjs/operators';
 import { getActions } from './action';
 
+const PARENT_HELLO_COMPONENT = new InjectionToken<HelloComponent>(
+  'Parent component'
+);
+
+function provideParentHelloComponent() {
+  return {
+    provide: PARENT_HELLO_COMPONENT,
+    useFactory: (cmp: HelloComponent) => cmp,
+    deps: [[HelloComponent, new SkipSelf(), new Optional()]],
+  };
+}
+
+export const injectParentHelloComponent = () => {
+  return ɵɵdirectiveInject(PARENT_HELLO_COMPONENT);
+};
+
 @Component({
   selector: 'hello',
   template: `
+  
     <div class="content">
+      <h3 *ngIf="parent; else noParent">My parent is {{parent.name}}</h3>
+      <ng-template #noParent>I don't have a parent</ng-template>
+
       <input type="text" [formControl]="control" />
       <button [disabled]="control.invalid" (click)="actions.add(control.value)">Add</button>
 
@@ -30,10 +54,9 @@ import { getActions } from './action';
      <hello
       *ngIf="show"
       [depth]="depth + 1"
+      [name]="name"
       [show]="false">
     </hello>
-
-  
   `,
   styles: [
     `
@@ -51,6 +74,7 @@ import { getActions } from './action';
 
   `,
   ],
+  providers: [provideParentHelloComponent()],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HelloComponent implements OnInit {
@@ -65,6 +89,8 @@ export class HelloComponent implements OnInit {
     Validators.required
   );
 
+  readonly parent = ɵɵdirectiveInject(PARENT_HELLO_COMPONENT);
+
   readonly actions = getActions<{
     add: string;
     delete: string;
@@ -72,6 +98,10 @@ export class HelloComponent implements OnInit {
 
   get showHideLabel() {
     return `${this.show ? 'Hide' : 'Show'} ${this.depth}`;
+  }
+
+  get name(): string {
+    return 'Hello component with depth: '  this.depth;
   }
 
   ngOnInit(): void {
