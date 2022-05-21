921bc0ba38c372218dfe81086ee2d43f2cc35d1b
riccardoperra <perrariccardo0@gmail.com>
2022-05-21 09:56:19 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,27 @@ import { ChangeDetectionStrategy, Component, VERSION } from '@angular/core';
 })
 export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  readonly links = [
+    {
+      title: `Example using a shared service between component, getAction helper and auto unsubscribe`,
+      name: 'With shared service',
+      href: '/todos',
+    },
+    {
+      title: `Example using nested providers and custom InjectFlags`,
+      name: 'Nested providers',
+      href: '/nested',
+    },
+    {
+      title: `Example using decorators, tracking change detection automatically in a component with detached view`,
+      name: 'Auto-track change detection with OnPush + detached view',
+      href: '/auto-cd',
+    },
+    {
+      title: `Example injecting dependencies outside constructor`,
+      name: 'Inject outside constructor',
+      href: '/inject-outside-constructor',
+    },
+  ];
 }
